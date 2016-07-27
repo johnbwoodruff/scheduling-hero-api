4315d35616aa96430044af39ceb8d68d43bf03bb
Andrew Tate <drewctate@gmail.com>
2016-07-26 20:31:57 -0600

    Adding event routes and DA functions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,8 @@
 var express = require('express'),
 	router = express.Router(),
 	Test = require('./test/test.controller'),
-	Users = require('./users/users.controller');
+	Users = require('./users/users.controller'),
+	Events = require('./events/events.controller');
 
 // TEST ROUTE
 router.get('/hello', Test.hello);
@@ -14,5 +15,12 @@ router.post('/users', Users.createUser);
 router.get('/users/:id', Users.getUserById);
 router.put('/users/:id', Users.updateUser);
 router.delete('/users/:id', Users.deleteUser);
+
+// EVENT ROUTES
+router.get('/events', Events.getAllEvents);
+router.post('/events', Events.createEvent);
+router.get('/events/:id', Events.getEventById);
+router.put('/events/:id', Events.updateEvent);
+router.delete('/events/:id', Events.deleteEvent);
 
 module.exports = router;
